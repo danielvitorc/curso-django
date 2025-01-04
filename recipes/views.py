cd15078b43b80abf89f2513d17695d1ff3b8a0ed
Daniel Vitor <danielvitorcz@gmail.com>
2025-01-04 15:29:06 -0400

    Exemplos de Template

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={
+        'name': 'Daniel'
+    })
 
 def sobre(request):
     return HttpResponse('Sobre')
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'temp.html')
