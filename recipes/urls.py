2df231f163b1704773171560a451894bfb1f034c
Daniel Vitor <danielvitorcz@gmail.com>
2025-01-04 15:45:04 -0400

    atualizações

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,4 @@ from recipes.views import home, sobre, contato
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'home.html', context={
+    return render(request, 'recipes/home.html', context={
         'name': 'Daniel'
     })
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return render(request, 'temp.html')
